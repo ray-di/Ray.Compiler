848a2beec0a6a4ae6ce2ddaf007bc9ae4d833000
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-16 11:28:43 +0900

    100% test coverage

---
+++ tests/NormalizerTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Ray.Compiler package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\Compiler;
+
+use PhpParser\Node\Scalar\String_;
+
+class NormalizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testString()
+    {
+        $normalizer = new Normalizer;
+        $string = $normalizer('ray');
+        /* @var $string String_ */
+        $this->assertInstanceOf(String_::class, $string);
+        $this->assertSame('ray', $string->value);
+    }
+
+    /**
+     * @expectedException \Ray\Compiler\Exception\InvalidInstance
+     */
+    public function testInvalidValue()
+    {
+        $normalizer = new Normalizer;
+        $resource = fopen(__FILE__, 'r');
+        $normalizer($resource);
+    }
+}
