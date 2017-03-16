f3a80752cd9cde9febeb02cabf704f0ffdc1788c
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-16 11:27:46 +0900

    extract method

--- src/Normalizer.php
+++ src/Normalizer.php
@@ -10,6 +10,7 @@ use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Scalar;
+use Ray\Compiler\Exception\InvalidInstance;
 use Ray\Di\InjectorInterface;
 
 /**
@@ -24,14 +25,10 @@ final class Normalizer
      * @param mixed $value The value to normalize
      *
      * @return Expr The normalized value
-     *
-     * @codeCoverageIgnore
      */
     public function __invoke($value)
     {
-        if ($value instanceof Node) {
-            return $value;
-        } elseif ($value === null) {
+        if ($value === null) {
             return new Expr\ConstFetch(
                 new Node\Name('null')
             );
@@ -45,29 +42,26 @@ final class Normalizer
             return new Scalar\DNumber($value);
         } elseif (is_string($value)) {
             return new Scalar\String_($value);
-        } elseif (is_array($value)) {
-            $items = [];
-            $lastKey = -1;
-            foreach ($value as $itemKey => $itemValue) {
-                // for consecutive, numeric keys don't generate keys
-                if (null !== $lastKey && ++$lastKey === $itemKey) {
-                    $items[] = new Expr\ArrayItem(
-                        $this->__invoke($itemValue)
-                    );
-                } else {
-                    $lastKey = null;
-                    $items[] = new Expr\ArrayItem(
-                        $this->__invoke($itemValue),
-                        $this->__invoke($itemKey)
-                    );
-                }
-            }
+        }
 
-            return new Expr\Array_($items);
+        return $this->noScalar($value);
+    }
+
+    /**
+     * Return array or object node
+     *
+     * @param mixed $value
+     *
+     * @return Expr\Array_|Expr\FuncCall
+     */
+    private function noScalar($value)
+    {
+        if (is_array($value)) {
+            return $this->arrayValue($value);
         } elseif (is_object($value)) {
             return $this->normalizeObject($value);
         }
-        throw new \LogicException('Invalid value');
+        throw new InvalidInstance; //@codeCoverageIgnore
     }
 
     /**
@@ -85,5 +79,34 @@ final class Normalizer
         $serialize = new Scalar\String_(serialize($object));
 
         return new Expr\FuncCall(new Node\Name('unserialize'), [new Arg($serialize)]);
+    }
+
+    /**
+     * Return array value node
+     *
+     * @param $value
+     *
+     * @return Expr\Array_
+     */
+    private function arrayValue($value)
+    {
+        $items = [];
+        $lastKey = -1;
+        foreach ($value as $itemKey => $itemValue) {
+            // for consecutive, numeric keys don't generate keys
+            if (null !== $lastKey && ++$lastKey === $itemKey) {
+                $items[] = new Expr\ArrayItem(
+                    $this->__invoke($itemValue)
+                );
+            } else {
+                $lastKey = null;
+                $items[] = new Expr\ArrayItem(
+                    $this->__invoke($itemValue),
+                    $this->__invoke($itemKey)
+                );
+            }
+        }
+
+        return new Expr\Array_($items);
     }
 }
