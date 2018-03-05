c7ae6aa96d798dac4a18eb9a986e875f1b9920e6
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-05 18:22:43 +0900

    add delete_dir function

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,24 +7,7 @@
 namespace Ray\Compiler;
 
 /* @var $loader \Composer\Autoload\ClassLoader */
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$loader = require \dirname(__DIR__) . '/vendor/autoload.php';
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+require \dirname(__DIR__) . '/vendor/autoload.php';
 
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
-// cleanup
-function clear($dir)
-{
-    $iterator = new \RecursiveIteratorIterator(
-        new \RecursiveDirectoryIterator($dir),
-        \RecursiveIteratorIterator::SELF_FIRST
-    );
-    foreach ($iterator as $file) {
-        /* @var $file \SplFileInfo */
-        if ($file->getFilename()[0] !== '.') {
-            @\unlink($file);
-        }
-    }
-}
-clear($_ENV['TMP_DIR']);
+delete_dir($_ENV['TMP_DIR']);
