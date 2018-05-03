a8979e611d90806cd1e3032f2868d3ee1d8c9de3
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-04 02:42:48 +0900

    fix cs

--- src/OnDemandCompiler.php
+++ src/OnDemandCompiler.php
@@ -57,7 +57,7 @@ final class OnDemandCompiler
         /** @var Dependency $dependency */
         $dependency = $containerArray[$dependencyIndex];
         $pointCuts = $this->loadPointcuts();
-        if ($dependency instanceof Dependency && is_array($pointCuts)) {
+        if ($dependency instanceof Dependency && \is_array($pointCuts)) {
             $dependency->weaveAspects(new Compiler($this->scriptDir), $pointCuts);
         }
         $code = (new DependencyCode($containerObject, $this->injector))->getCode($dependency);
