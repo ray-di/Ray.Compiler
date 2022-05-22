5876874f3e8b91b20afa0318c7e72c63871a4464
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-05-22 15:17:52 +0900

    Add InjectorContext examples

---
+++ docs/exmaple/TestInjectorContext.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Common\Cache\ApcuCache;
+use Doctrine\Common\Cache\CacheProvider;
+use Ray\Compiler\AbstractInjectorContext;
+use Ray\Compiler\DiCompileModule;
+use Ray\Compiler\FakeCarModule;
+use Ray\Di\AbstractModule;
+use Ray\Di\NullCache;
+
+final class TestInjectorContext extends AbstractInjectorContext
+{
+    public function getModule(): AbstractModule
+    {
+        $module = new FakeCarModule();
+        $module->override(new TestModule());
+
+        return $module;
+    }
+
+    public function getCache(): CacheProvider
+    {
+        return new NullCache();
+    }
+}
