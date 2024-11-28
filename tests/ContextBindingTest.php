060b835232828740cbc50197498767d770158624
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-28 11:10:43 +0900

    Refactor ContextBindingTest to use a CompileInjector
    The private injector property was introduced and initialized in the setUp method to prevent redundant instantiation. This ensures cleaner code and reduces duplication by reusing the same injector across multiple tests.

--- tests/ContextBindingTest.php
+++ tests/ContextBindingTest.php
@@ -8,30 +8,30 @@ use PHPUnit\Framework\TestCase;
 
 final class ContextBindingTest extends TestCase
 {
+    private $injector;
+
     public function setUp(): void
     {
         deleteFiles(__DIR__ . '/tmp');
+        $this->injector = new CompileInjector(
+            __DIR__ . '/tmp',
+            LazyModule::getInstance(static function () {
+                return new FakeDependContextualRobotModule('');
+            })
+        );
     }
 
     /** @requires PHP >= 7.4 */
     public function testContextBindingWhenContextIsEmptyAndPropertyHasType(): void
     {
-        $injector = new ScriptInjector(__DIR__ . '/tmp', static function () {
-            return new FakeDependContextualRobotModule('');
-        });
-
-        $instance = $injector->getInstance(FakeRobotInterface::class);
+        $instance = $this->injector->getInstance(FakeRobotInterface::class);
         $this->assertInstanceOf(FakeRobotInterface::class, $instance);
     }
 
     /** @requires PHP >= 7.4 */
     public function testContextBindingWhenContextIsEmpty(): void
     {
-        $injector = new ScriptInjector(__DIR__ . '/tmp', static function () {
-            return new FakeContextualModule('');
-        });
-
-        $instance = $injector->getInstance(FakeRobotInterface::class);
+        $instance = $this->injector->getInstance(FakeRobotInterface::class);
         $this->assertInstanceOf(FakeRobotInterface::class, $instance);
     }
 }
