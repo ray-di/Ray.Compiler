428d55ab5b83bf48634b9e0434ecfb4f230341f2
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-28 11:06:26 +0900

    Add LazyModule class for creating a lazy module
    This class provides a factory method to generate instances of LazyModuleInterface. It is intended for testing purposes and should not be used in production, as it allows creating a module from a callable variable.

---
+++ src/LazyModule.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\Compiler;
+
+use Ray\Di\AbstractModule;
+
+/**
+ * Factory class for creating a lazy module
+ *
+ * Lazymodule is required to create CompileInjector. This utility class is useful when creating a module
+ * from a variable that can be called for testing purposes.
+ *
+ * Please do not use this in production code. Instead of using this class, please create a class that implements LazyModuleInterface.
+ */
+final class LazyModule
+{
+    /**
+    public static function getInstance(callable $callable): LazyModuleInterface
+    {
+        return new class ($callable) implements LazyModuleInterface {
+            /** @var callable */
+            private $callable;
+
+            public function __construct(callable $callable)
+            {
+                $this->callable = $callable;
+            }
+
+            public function __invoke(): AbstractModule
+            {
+                return ($this->callable)();
+            }
+        };
+    }
+}
