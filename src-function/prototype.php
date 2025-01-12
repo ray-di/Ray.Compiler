d9a93818ff6e1366d81a2668878f765492cfe7e9
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-01-12 14:47:50 +0900

    Update src-function/prototype.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src-function/prototype.php
+++ src-function/prototype.php
@@ -13,8 +13,13 @@ use function file_exists;
  * @return mixed
  */
 function prototype(string $scriptDir, string $filePath, ?array $ip = null) {
-    $file = $scriptDir . $filePath;
-    assert(file_exists($file)); // should be resolvable
+    $file = realpath($scriptDir) . DIRECTORY_SEPARATOR . ltrim($filePath, '/\\');
+    if (!$file || !file_exists($file)) {
+        throw new \RuntimeException(sprintf('File not found: %s', $filePath));
+    }
+    if (!str_starts_with($file, realpath($scriptDir))) {
+        throw new \RuntimeException('Path traversal detected');
+    }
 
     // $ip can be used in the included file
     return require $file;
